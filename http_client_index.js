4f5c80b82983dc79666ca40595564a41f8921464
David Pal <paldavid2030@gmail.com>
2019-07-22 11:29:32 +0300

    weed out http client code

--- http_client_index.js
+++ http_client_index.js
@@ -1,14 +1,7 @@
 'use strict'
 const http = require('http');
 
-// http.get(process.argv[2], response => {
-//   response.setEncoding('utf8');
-//   response.on('data', (data) => console.log(data));
-// })
-
-// console.log(http.STATUS_CODES);
-
-http.createServer((req, res) => {
-  res.write('hello world');
-  res.end()
-}).listen(8888, () => console.log('app listening on port 8888'));
+http.get(process.argv[2], response => {
+  response.setEncoding('utf8');
+  response.on('data', (data) => console.log(data));
+})
