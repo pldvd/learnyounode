879d1a11d1974e8218948cf562fb1966883537c7
David Pal <paldavid2030@gmail.com>
2019-07-22 13:13:43 +0300

    add unix time parsing to JSON API server

--- JSON_API_server.js
+++ JSON_API_server.js
@@ -5,11 +5,21 @@ const server = http.createServer((req, res) => {
   const requestUrl = url.parse(req.url, true);
 
   if (requestUrl.pathname === '/api/parsetime' && req.method === 'GET') {
-    const dateSent = new Date(requestUrl.query.iso);
+    let dateSent = new Date(requestUrl.query.iso);
+    console.log(dateSent);
     const response = {
       hour: dateSent.getHours(),
       minute: dateSent.getMinutes(),
       second: dateSent.getSeconds()
+    }
+
+    res.writeHead(200, { 'Content-Type': 'application/JSON' });
+    res.end(JSON.stringify(response));
+  }
+  if (requestUrl.pathname === '/api/unixtime' && req.method === 'GET') {
+    let dateSent = new Date(requestUrl.query.iso);
+    const response = {
+      unixtime: dateSent.valueOf(),
     }
 
     res.writeHead(200, { 'Content-Type': 'application/JSON' });
