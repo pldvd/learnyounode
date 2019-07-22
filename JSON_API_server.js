ec61e38902f814793e0dd98896d717a67966d563
David Pal <paldavid2030@gmail.com>
2019-07-22 13:00:10 +0300

    add date parser functionality to JSON API server

---
+++ JSON_API_server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+  const requestUrl = url.parse(req.url, true);
+
+  if (requestUrl.pathname === '/api/parsetime' && req.method === 'GET') {
+    const dateSent = new Date(requestUrl.query.iso);
+    const response = {
+      hour: dateSent.getHours(),
+      minute: dateSent.getMinutes(),
+      second: dateSent.getSeconds()
+    }
+
+    res.writeHead(200, { 'Content-Type': 'application/JSON' });
+    res.end(JSON.stringify(response));
+  }
+})
+
+server.listen(process.argv[2], () => console.log(`listening on port ${process.argv[2]}`));
