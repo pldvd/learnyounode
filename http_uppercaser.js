da9fb8f06c10a90bc1e78f8f6004952f028eaa7d
David Pal <paldavid2030@gmail.com>
2019-07-22 12:29:29 +0300

    add req.method restriction to server

--- http_uppercaser.js
+++ http_uppercaser.js
@@ -1,24 +1,32 @@
 const http = require('http');
 
-const server = http.createServer((req,res) => {
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST') {
+    let body = '';
+    req.setEncoding('utf8');
 
-  let body = '';
-  req.setEncoding('utf8');
+    req.on('data', (chunk) => {
+      body += chunk.toUpperCase();
+    })
 
-  req.on('data', (chunk) => {
-    body += chunk.toUpperCase();
-  })
-
-  req.on('end', () => {
-    try {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.write(body);
-      res.end();
-    } catch(e) {
-      res.statusCode = 400;
-      return res.end(`Error: ${e.message}`);
-    }
-  })
+    req.on('end', () => {
+      try {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write(body);
+        res.end();
+      } catch (e) {
+        res.statusCode = 400;
+        return res.end(`Error: ${e.message}`);
+      }
+    })
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.write('send me a post');
+    res.end();
+  }
 })
 
-server.listen(process.argv[2]);
+
+server.listen(process.argv[2], () => {
+  console.log(`listening on ${process.argv[2]}`);
+});
